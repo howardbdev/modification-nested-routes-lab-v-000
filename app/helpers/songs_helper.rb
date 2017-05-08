d29cbd11cf398566033e981af439055e9838273a
Howard DeVennish <howarddevennish@gmail.com>
2017-05-08 22:34:38 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
+  def artist_select(song)
 
+  end
+  # Previous Lesson:
+  # def author_id_field(post)
+  #   if post.author.nil?
+  #     select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+  #   else
+  #     hidden_field_tag "post[author_id]", post.author_id
+  #   end
+  # end
 end
